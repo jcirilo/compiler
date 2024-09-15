1568f934016732e6f2c8d821111e5fc505d0e244
João V. S. Cirilo <jvscirilo12@gmail.com>
2024-09-15 17:10:38 -0300

    Main adapitada para testes

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,24 +1,27 @@
 package main;
 
-import java.util.ArrayList;
-
-import lexical.Lexical;
-import lexical.Token;
+import lexical.Scanner;
 import parser.Parser;
 
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayList<Token> buffer;
-		Lexical lexicalAnalyzer = new Lexical();
-		Parser parserAnalyzer = new Parser();
-		String path = "exemplo.pas";
-		
+		test("/test/Test1.pas");
+		test("/test/Test2.pas");
+		test("/test/Test3.pas");
+		test("/test/Test4.pas");
+		test("/test/Test5.pas");
+		test("/test/Test6.pas");
+		test("/test/Test7.pas");
+	}
+
+	public static void test(String fileName) {
 		try {
-			buffer = lexicalAnalyzer.tokenizer(path);
-			parserAnalyzer.parse(buffer);
-		} catch (RuntimeException e ){
-			System.out.println(e.getMessage());
+			String path = "%s/%s".formatted(System.getProperty("user.dir"), fileName);
+			new Parser().parse(new Scanner(path).tokenizer());
+			System.out.println(fileName + "\t\t" + "compilation sucessfully");
+		} catch (RuntimeException e) {
+			System.out.println(fileName + "\t\t" + e.getLocalizedMessage());
 		}
 	}
 
